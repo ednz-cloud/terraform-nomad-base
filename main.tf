77b2c467637c0e2b3fa251dd768a28fd39a54cc9
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-07-16 13:24:45 +0200

    moved module from monorepo to independent repo

--- main.tf
+++ main.tf
@@ -1,1 +1,70 @@
 # main file for terraform-nomad-base
+resource "nomad_job" "this" {
+  for_each = var.jobs
+  depends_on = [
+    nomad_external_volume.this,
+    consul_key_prefix.this,
+    # consul_config_entry.this
+    consul_intention.this
+  ]
+
+  jobspec          = file("${each.value}")
+  purge_on_destroy = true
+  hcl2 {
+    enabled  = true
+    allow_fs = true
+  }
+}
+
+resource "nomad_external_volume" "this" {
+  for_each = var.volumes
+
+  volume_id    = each.key
+  name         = each.key
+  type         = each.value.type
+  plugin_id    = each.value.plugin_id
+  namespace    = each.value.namespace
+  capacity_min = each.value.capacity_min
+  capacity_max = each.value.capacity_max
+  capability {
+    access_mode     = each.value.capability.access_mode
+    attachment_mode = each.value.capability.attachment_mode
+  }
+  parameters = sensitive(each.value.parameters)
+  secrets    = sensitive(each.value.secrets)
+}
+
+resource "nomad_volume" "this" {
+  for_each = var.nfs_volumes
+
+  volume_id   = each.key
+  external_id = each.key
+  name        = each.key
+  type        = each.value.type
+  plugin_id   = each.value.plugin_id
+  namespace   = each.value.namespace
+  capability {
+    access_mode     = each.value.capability.access_mode
+    attachment_mode = each.value.capability.attachment_mode
+  }
+  context = each.value.context
+  mount_options {
+    fs_type     = each.value.mount_options.fs_type
+    mount_flags = each.value.mount_options.mount_flags
+  }
+}
+
+resource "consul_key_prefix" "this" {
+  for_each = var.consul_kv
+
+  path_prefix = each.key
+  subkeys     = each.value
+}
+
+resource "consul_intention" "this" {
+  for_each = var.consul_intentions
+
+  source_name      = each.value.source_name
+  destination_name = each.value.destination_name
+  action           = each.value.action
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,53 @@
 # variables file for terraform-nomad-base
+variable "jobs" {
+  type        = map(string)
+  description = "The list of jobs to deploy"
+  default     = {}
+}
+
+variable "volumes" {
+  type = map(object({
+    type         = string
+    plugin_id    = string
+    namespace    = string
+    capacity_min = string
+    capacity_max = string
+    capability   = map(string)
+    parameters   = map(string)
+    secrets      = map(string)
+  }))
+  description = "List of volumes associated to the job(s)"
+  default     = {}
+}
+
+variable "nfs_volumes" {
+  type = map(object({
+    type       = string
+    plugin_id  = string
+    namespace  = string
+    capability = map(string)
+    context    = map(string)
+    mount_options = object({
+      fs_type     = string
+      mount_flags = list(string)
+    })
+  }))
+  description = "List of nfs volumes associated to the job(s)"
+  default     = {}
+}
+
+variable "consul_kv" {
+  type        = map(any)
+  description = "List key/value pairs to put at a specific prefix (mainly for traefik)"
+  default     = {}
+}
+
+variable "consul_intentions" {
+  type = map(object({
+    source_name      = string
+    destination_name = string
+    action           = string
+  }))
+  description = "List of intentions associated with the job(s)"
+  default     = {}
+}
