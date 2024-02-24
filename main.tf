7d683b2a6acf4688b0138931e9b32f79408f9649
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-02-25 00:05:59 +0100

    fix(job): wrong variable use in nomad job

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "nomad_job" "this" {
   purge_on_destroy = true
   hcl2 {
     allow_fs = true
-    vars     = contains(keys(var.jobs_variables), each.key) ? jobs_variables[each.key] : {}
+    vars     = contains(keys(var.jobs_variables), each.key) ? var.jobs_variables[each.key] : {}
   }
 }
 
